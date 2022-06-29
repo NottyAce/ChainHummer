9376680b5df03e5ed92966fcc5f16a2d13b8b4d1
Noto <39573156+NottyAce@users.noreply.github.com>
2022-06-29 22:50:12 +0900

    InHotel
    ホテルでカメラに色々加えようとしたやつ

--- ChainHummer/Assets/Scripts/GameManager.cs
+++ ChainHummer/Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject mainCamera;
+
     // Start is called before the first frame update
     void Start()
     {
